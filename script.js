486dcc6d692f5b6a0be10f7687081619a162e048
Your Nameeli <elighidiu@yahoo.com>
2021-08-02 11:15:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//CAROUSEL
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  // select all elements with class name mySlides
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+
+Array.from(document.querySelectorAll(".letter")).forEach(el => {
+   el.innerText = randomLetter();
+});
+
+function randomLetter(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random() * alphabet.length)]
+}
+
+
+
+
