d109e407fa35998060dc4504bdc0cfec60f59685
Your Nameeli <elighidiu@yahoo.com>
2021-08-03 14:08:19 +0200

    parallax effect

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
+
 //CAROUSEL
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
+  let i;
   // select all elements with class name mySlides
-  var slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("mySlides");
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].classList.remove("active"); //Removing class active from elements which are not visible
+
+
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active"); // Added class active on the visible element
+
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+
 }
 
+//Parallax effect on carousel
+window.addEventListener("scroll", () => {
+  let active = document.querySelector(".active");
+  let scrollValue = window.scrollY;
 
-Array.from(document.querySelectorAll(".letter")).forEach(el => {
-   el.innerText = randomLetter();
+  active.style.transform = `translateY(-${scrollValue * 0.5}px)`;
+
 });
 
-function randomLetter(){
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random() * alphabet.length)]
+Array.from(document.querySelectorAll(".letter")).forEach(el => {
+  el.innerText = randomLetter();
+});
+
+function randomLetter() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
 
